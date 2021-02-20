f12ab2797ed5129708e3af373701772195203738
savin-dev <74615072+savin-dev@users.noreply.github.com>
2021-02-20 16:09:37 +0530

    first task completed

--- aboutus.php
+++ aboutus.php
@@ -1,11 +1,17 @@
 <?php
 $pagename="homteq: cloud controlled tech for your home"; //Create and populate a variable called $pagename
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>"; //Call in stylesheet
+
 echo "<title>".$pagename."</title>"; //display name of the page as window title
+
 echo "<body>";
+
 include ("headfile.html"); //include header layout file
+
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
+
 //display random text
+
 echo "<p>
 <p class='updateInfo'> homteq is a highly-specialised online retailer that offers a wide range of devices at the most competitive prices
 to make your home and your life super SMART.
@@ -45,6 +51,8 @@ them.
 <p class='updateInfo'>
 Whatever smart tech you are after, homteq has it on offer!
 </p>";
+
 include("footfile.html"); //include head layout
+
 echo "</body>";
 ?>
